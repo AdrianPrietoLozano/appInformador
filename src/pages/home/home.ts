5290e22f33cad2c222a6e809a2733bd4112e80cf
Adrian <prietolozano.adrian@gmail.com>
2019-04-03 00:35:54 -0600

    Primeras pruebas

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  casas = [];
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public http: HttpClient) {
+    this.http.get("/casas")
+    .subscribe(data => {
+      console.log(JSON.stringify(data));
+    }, error => {
+      console.log(JSON.stringify(error));
+    });
 
+    console.log(this.casas);
   }
 
 }
